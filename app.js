2d3a7fce4c68f746a0f0b30775af9434d9cf321b
wiltrahan <twilorip@gmail.com>
2017-08-04 16:39:14 -0400

    operator click function set

--- app.js
+++ app.js
@@ -11,5 +11,20 @@ $(document).ready(function() {
         total.text(number);
     });
 
+  $(".operators").click(function() {
+    operator = $(this).text();
+    secondNumber = number;
+    number = "";
+    total.text(secondNumber);
+
+  });
+
+  $("#clear, #all_clear").click(function() {
+    number = "";
+    total.text("0");
+    if($(this).attr("id") === "all_clear") {
+      secondNumber = "";
+    }
+  });
 
 });
