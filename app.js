903fbf8a414de278ea2414b733a6ca3f4e0b8880
wiltrahan <twilorip@gmail.com>
2017-08-04 16:56:59 -0400

    equal & switch set

--- app.js
+++ app.js
@@ -2,29 +2,51 @@ $(document).ready(function() {
   var number = "";
   var secondNumber = "";
   var operator = "";
-  var total = $("#totalWindow")
+  var total = $("#totalWindow");
 
   total.text("0");
 
   $(".numbers").click(function() {
-        number += $(this).text();
-        total.text(number);
-    });
+    number += $(this).text();
+    total.text(number);
+  });
 
   $(".operators").click(function() {
     operator = $(this).text();
     secondNumber = number;
     number = "";
     total.text(secondNumber);
-
   });
 
   $("#clear, #all_clear").click(function() {
     number = "";
     total.text("0");
-    if($(this).attr("id") === "all_clear") {
+    if ($(this).attr("id") === "all_clear") {
       secondNumber = "";
     }
   });
 
+  $("#equals").click(function() {
+    switch (operator) {
+      case "+":
+        number = parseInt(secondNumber, 10) + parseInt(number, 10);
+        number = number.toString(10);
+        break;
+      case "-":
+        number = parseInt(secondNumber, 10) - parseInt(number, 10);
+        number = number.toString(10);
+        break;
+      case "/":
+        number = parseInt(secondNumber, 10) / parseInt(number, 10);
+        number = number.toString(10);
+        break;
+      case "*":
+        number = parseInt(secondNumber, 10) * parseInt(number, 10);
+        number = number.toString(10);
+        break;
+    }
+
+    total.text(number);
+    secondNumber = "";
+  });
 });
